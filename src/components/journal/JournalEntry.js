1a8f44ab7844e1edd1438ba7bebaee09772a5100
IchBinKmt <80807417+IchBinKmt@users.noreply.github.com>
2021-09-13 08:33:50 -0500

    first commit

---
+++ src/components/journal/JournalEntry.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const JournalEntry = () => {
+    return (
+        <div className="journal__entry">
+            <div 
+                className="journal__entry-picture"
+                style={{
+                    backgroundSize: 'cover',
+                    backgroundImage: 'url(https://image.winudf.com/v2/image1/Y29tLmZyZXNoZW52aXJvbm1lbnQuY29vbHdhbGxwYXBlcnNoZF9zY3JlZW5fMF8xNTk4OTcwMDU3XzA3MQ/screen-0.jpg?fakeurl=1&type=.jpg)',
+                }}
+            >
+            </div>
+        </div>
+    )
+}
